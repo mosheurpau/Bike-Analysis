3116be61e0362261aa135dd954f684101fd9a86f
mosheurpau <mosheur.pau@gamil.com>
2022-04-04 05:57:00 +0600

    added router and nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Reviews from "./components/Reviews/Reviews";
+import Blogs from "./components/Blogs/Blogs";
+import About from "./components/About/About";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to React Router!</h1>
+      <Header></Header>
       <Routes>
-        <Route path="/" element={<Header></Header>} />
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/revews" element={<Reviews></Reviews>}></Route>
+        <Route path="/dashboard" element={<Blogs></Blogs>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
       </Routes>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
   return (
     <div>
-      <h2>This is Header</h2>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="">Bike Analysis</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto">
+              <Link to="/home">Home</Link>
+              <Link to="/revews">Revews</Link>
+              <Link to="/dashboard">Dashboard</Link>
+              <Link to="/about">About</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
