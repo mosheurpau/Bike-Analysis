f7d98e742edae23600c9a9ed6c488ef74d554e4d
mosheurpau <mosheur.pau@gamil.com>
2022-04-04 15:13:18 +0600

    added banner img and text

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import bike from "../../Images/bike-bg.png";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
-      <h2>This is Home</h2>
-    </div>
+    <Container fluid>
+      <Row className="banner-bg">
+        <Col sm={5} className="banner-text">
+          <h1 className="bike-name">NINJA H2®R</h1>
+          <h1>BUILT BEYOND BELIEF</h1>
+          <p>
+            Surrounding the Ninja H2®R motorcycle’s high-output engine is a
+            multitude of premium, high-performance components, selected
+            specifically for unmatchable performance at track speeds.
+          </p>
+          <button>Live demo</button>
+        </Col>
+        <Col sm={7} className="banner-img">
+          <img src={bike} alt=""></img>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
