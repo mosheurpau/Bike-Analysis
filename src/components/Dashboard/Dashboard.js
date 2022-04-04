0741a31143f59adaf0ffe2d4367bb5c74c64048b
mosheurpau <mosheur.pau@gamil.com>
2022-04-05 01:04:17 +0600

    added some style dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -15,6 +15,7 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
+import "./Deshboard.css";
 
 const Dashboard = () => {
   const data = [
@@ -56,11 +57,11 @@ const Dashboard = () => {
     },
   ];
   return (
-    <Container>
+    <Container className="dashboard-container">
       <Row>
         <Col sm={6}>
-          <h5>Month wish Sell</h5>
-          <div>
+          <h5 className="my-4">Month wish Sell</h5>
+          <div className="d-flex justify-content-center">
             <LineChart width={400} height={300} data={data}>
               <Line type="monotone" dataKey={"sell"} stroke="#FF0000"></Line>
               <XAxis dataKey="month"></XAxis>
@@ -71,82 +72,88 @@ const Dashboard = () => {
           </div>
         </Col>
         <Col sm={6}>
-          <h5>Investment VS Revenue</h5>
-          <AreaChart
-            width={400}
-            height={300}
-            data={data}
-            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-          >
-            <defs>
-              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-              </linearGradient>
-              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-              </linearGradient>
-            </defs>
-            <XAxis dataKey="month" />
-            <YAxis />
-            <CartesianGrid strokeDasharray="3 3" />
-            <Tooltip />
-            <Area
-              type="monotone"
-              dataKey="investment"
-              stroke="#8884d8"
-              fillOpacity={1}
-              fill="url(#colorUv)"
-            />
-            <Area
-              type="monotone"
-              dataKey="revenue"
-              stroke="#82ca9d"
-              fillOpacity={1}
-              fill="url(#colorPv)"
-            />
-          </AreaChart>
+          <h5 className="my-4">Investment VS Revenue</h5>
+          <div className="d-flex justify-content-center">
+            <AreaChart
+              width={400}
+              height={300}
+              data={data}
+              margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+            >
+              <defs>
+                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                  <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                </linearGradient>
+                <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                  <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                </linearGradient>
+              </defs>
+              <XAxis dataKey="month" />
+              <YAxis />
+              <CartesianGrid strokeDasharray="3 3" />
+              <Tooltip />
+              <Area
+                type="monotone"
+                dataKey="investment"
+                stroke="#8884d8"
+                fillOpacity={1}
+                fill="url(#colorUv)"
+              />
+              <Area
+                type="monotone"
+                dataKey="revenue"
+                stroke="#82ca9d"
+                fillOpacity={1}
+                fill="url(#colorPv)"
+              />
+            </AreaChart>
+          </div>
         </Col>
       </Row>
       <Row>
         <Col sm={6}>
-          <h5>Investment VS Revenue</h5>
-          <BarChart width={400} height={300} data={data}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="month" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="investment" fill="#8884d8" />
-            <Bar dataKey="revenue" fill="#82ca9d" />
-          </BarChart>
+          <h5 className="my-4">Investment VS Revenue</h5>
+          <div className="d-flex justify-content-center">
+            <BarChart width={400} height={300} data={data}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="month" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="investment" fill="#8884d8" />
+              <Bar dataKey="revenue" fill="#82ca9d" />
+            </BarChart>
+          </div>
         </Col>
         <Col sm={6}>
-          <h5>Investment VS Revenue</h5>
-          <PieChart width={730} height={250}>
-            <Pie
-              data={data}
-              dataKey="investment"
-              nameKey="month"
-              cx="50%"
-              cy="50%"
-              outerRadius={50}
-              fill="#8884d8"
-            />
-            <Pie
-              data={data}
-              dataKey="revenue"
-              nameKey="month"
-              cx="50%"
-              cy="50%"
-              innerRadius={60}
-              outerRadius={80}
-              fill="#82ca9d"
-              label
-            />
-            <Tooltip />
-          </PieChart>
+          <h5 className="my-4">Investment VS Revenue</h5>
+          <div className="d-flex justify-content-center">
+            <PieChart width={730} height={250}>
+              <Pie
+                data={data}
+                dataKey="investment"
+                nameKey="month"
+                cx="50%"
+                cy="50%"
+                outerRadius={50}
+                fill="#8884d8"
+              />
+              <Pie
+                data={data}
+                dataKey="revenue"
+                nameKey="month"
+                cx="50%"
+                cy="50%"
+                innerRadius={60}
+                outerRadius={80}
+                fill="#82ca9d"
+                label
+              />
+              <Tooltip />
+            </PieChart>
+          </div>
         </Col>
       </Row>
     </Container>
