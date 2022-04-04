183a61e62dcd5c16d213cba560f9f80d0bb8fe55
mosheurpau <mosheur.pau@gamil.com>
2022-04-05 00:24:45 +0600

    added AreaChart

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,15 @@
 import React from "react";
-import { Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
+import { Col, Container, Row } from "react-bootstrap";
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 const Dashboard = () => {
   const data = [
@@ -41,13 +51,68 @@ const Dashboard = () => {
     },
   ];
   return (
-    <LineChart width={400} height={300} data={data}>
-      <Line dataKey={"sell"}></Line>
-      <XAxis dataKey="month"></XAxis>
-      <Tooltip />
-      <YAxis></YAxis>
-      <h3>Sell</h3>
-    </LineChart>
+    <Container>
+      <Row>
+        <Col sm={6}>
+          <h5>Month wish Sell</h5>
+          <div>
+            <LineChart width={400} height={300} data={data}>
+              <Line type="monotone" dataKey={"sell"} stroke="#FF0000"></Line>
+              <XAxis dataKey="month"></XAxis>
+              <Tooltip />
+              <YAxis></YAxis>
+              <h3>Sell</h3>
+            </LineChart>
+          </div>
+        </Col>
+        <Col sm={6}>
+          <h5>Investment VS Revenue</h5>
+          <AreaChart
+            width={400}
+            height={300}
+            data={data}
+            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+          >
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+              </linearGradient>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <XAxis dataKey="month" />
+            <YAxis />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="investment"
+              stroke="#8884d8"
+              fillOpacity={1}
+              fill="url(#colorUv)"
+            />
+            <Area
+              type="monotone"
+              dataKey="revenue"
+              stroke="#82ca9d"
+              fillOpacity={1}
+              fill="url(#colorPv)"
+            />
+          </AreaChart>
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={6}>
+          <h5>Investment VS Revenue</h5>
+        </Col>
+        <Col sm={6}>
+          <h5>Investment VS Revenue</h5>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
